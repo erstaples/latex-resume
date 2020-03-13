d134767409aaa75098082a93b50725dee3606b8e
erstaples <erstaples@gmail.com>
2020-03-12 19:12:07 -0700

    update resume

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ IMAGE=blang/latex:ubuntu
 docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" \
     latexmk -xelatex -cd -f -interaction=batchmode -pdf resume.tex
 
+datetime=$(date +%Y-%m-%d-%H.%M.%S)
+cp resume.pdf $HOME/Documents/eric-staples-resume-$datetime.pdf 
+cp resume.pdf $HOME/Documents/eric-staples-resume--latest.pdf
 open resume.pdf
